fb6cce557128d8082a04b924b573fb178322d68d
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-02-19 13:12:17 +0300

    vscode tried to ruin my code, fixed

--- multiclient/chat_client.py
+++ multiclient/chat_client.py
@@ -1,5 +1,5 @@
 from socket import socket
-from multiclient.chat_server import ADDRESS, PORT, DISCONNECT, AUTH_OK, decode, encode
+from chat_server import ADDRESS, PORT, DISCONNECT, AUTH_OK, decode, encode
 
 
 def auth(client: socket) -> bool:
