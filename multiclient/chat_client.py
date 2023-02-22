4a25f01d151db12660a51abddd4ad851984af6e2
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-02-22 10:34:54 +0300

    multiclient threading stable

--- multiclient/chat_client.py
+++ multiclient/chat_client.py
@@ -1,5 +1,15 @@
 from socket import socket
-from chat_server import ADDRESS, PORT, DISCONNECT, AUTH_OK, decode, encode
+from chat_server import decode, encode
+from threading import Thread
+from dotenv import load_dotenv
+from os import getenv
+from argparse import ArgumentParser
+from sys import argv
+
+
+load_dotenv()
+DISCONNECT = getenv('DISCONNECT')
+AUTH_OK = getenv('AUTH_OK')
 
 
 def auth(client: socket) -> bool:
@@ -19,25 +29,48 @@ def auth(client: socket) -> bool:
         print(f'Server response: {answer}')
 
 
+def sender(client: socket):
+    while True:
+        msg = input()
+        client.send(encode(msg))
+        if msg == DISCONNECT:
+            break
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('-a', '--address', default='127.0.0.1')
+    parser.add_argument('-p', '--port', default='8001')
+    args = parser.parse_args(argv[1:])
+    try:
+        port = int(args.port)
+    except Exception as error:
+        print(f'Error occured while parsing args: {error}')
+        return args.address, 8001
+    else:
+        return args.address, port
+
+
 def main(client: socket) -> None:
-    client.connect((ADDRESS, PORT))
+    client.connect(get_args())
     if auth(client):
+        Thread(target=sender, args=(client,), daemon=True).start()
         while True:
-            msg = input('Your msg: ')
-            client.send(encode(msg))
-            if msg == DISCONNECT:
+            server_msg = decode(client.recv(1024))
+            if server_msg == DISCONNECT:
                 break
+            print(server_msg)
 
 
 client = socket()
 try:
     main(client)
 except KeyboardInterrupt:
-    print('Мог дисконнект написать?')
+    print(f'use {DISCONNECT} next time')
     client.send(encode(DISCONNECT))
 except BrokenPipeError as error:
-    print(f'Всё сломалось {error.strerror}')
+    print(f'error (BrokenPipe) occured: {error.strerror}')
 except Exception as error:
-    print('Какаята ещё ошибка', error)
+    print(f'error occured: {error}')
 finally:
     client.close()
--- multiclient/chat_server.py
+++ multiclient/chat_server.py
@@ -2,29 +2,32 @@ from socket import socket
 from dotenv import load_dotenv
 import os
 from threading import Thread, Lock
+from types import SimpleNamespace
 
+
+consts = SimpleNamespace()
 load_dotenv()
 
-ADDRESS = os.getenv('ADDRESS')
-DISCONNECT = os.getenv('DISCONNECT')
-ENCODING = os.getenv('ENCODING')
-AUTH_OK = os.getenv('AUTH_OK')
-GREETING = os.getenv('GREETING')
-SHUTDOWN: str = os.getenv('SHUTDOWN', default='/shutdown')
-HELP: str = os.getenv('HELP', default='/help')
-LIST: str = os.getenv('LIST', default='/list')
+consts.ADDRESS = os.getenv('ADDRESS')
+consts.DISCONNECT = os.getenv('DISCONNECT')
+consts.ENCODING = os.getenv('ENCODING')
+consts.AUTH_OK = os.getenv('AUTH_OK')
+consts.GREETING = os.getenv('GREETING')
+consts.SHUTDOWN: str = os.getenv('SHUTDOWN', default='/shutdown')
+consts.HELP: str = os.getenv('HELP', default='/help')
+consts.LIST: str = os.getenv('LIST', default='/list')
 try:
-    PORT = int(os.getenv('PORT'))
+    consts.PORT = int(os.getenv('PORT'))
 except Exception as error:
     print(f'Error occured while loading PORT:{error} \n, defaults to 8001')
-    PORT = 8001
+    consts.PORT = 8001
 
 
-def encode(text: str, coding=ENCODING) -> bytes:
+def encode(text: str, coding=consts.ENCODING) -> bytes:
     return text.encode(coding)
 
 
-def decode(msg: bytes, coding=ENCODING) -> str:
+def decode(msg: bytes, coding=consts.ENCODING) -> str:
     return msg.decode(coding)
 
 
@@ -34,25 +37,47 @@ def users_enum():
         return '\n'.join([f'{num}. {user}' for num, user in enumerate(list(users.keys()))])
 
 
+def parse_msg(msg: str, name: str, client: socket) -> bool:
+    global users, users_lock
+    match msg:
+        case str(consts.DISCONNECT):
+            client.send(encode(consts.DISCONNECT))
+            client.close()
+            with users_lock:
+                if name in users.keys():
+                    del users[name]
+            print(f'Client {name} has disconnected')
+            return False
+        case str(consts.LIST):
+            client.send(encode(users_enum()))
+        case _:
+            msg = f'{name}: {msg}'
+            print(msg)
+            with users_lock:
+                for client in users.values():
+                    client.send(encode(msg))
+    return True
+
+
 def receiver(client: socket, name: str):
     while True:
-        print(f'{name}: {decode(client.recv(1024))}')
+        msg = decode(client.recv(1024))
+        if not parse_msg(msg, name, client):
+            break
 
 
 def new_client(client: socket, cl_address: tuple) -> str:
     global users, users_lock
-    client.send(encode(GREETING))
+    client.send(encode(consts.GREETING))
     while True:
         msg = decode(client.recv(1024))
-        if msg == DISCONNECT:
-            client.send(encode(DISCONNECT))
-            print(f'Client {cl_address} has disconnected')
+        if not parse_msg(msg, str(cl_address), client):
             return
         with users_lock:
             if msg in users.keys():
                 client.send(encode('Username is taken'))
             else:
-                client.send(encode(AUTH_OK))
+                client.send(encode(consts.AUTH_OK))
                 print(f'Client {cl_address} authenticated by name {msg}')
                 users[msg] = client
                 Thread(target=receiver, args=(client, msg)).start()
@@ -67,23 +92,23 @@ def accept_client(server: socket):
 
 
 def main(server: socket) -> None:
-    server.bind((ADDRESS, PORT))
+    server.bind((consts.ADDRESS, consts.PORT))
     server.listen()
     Thread(target=accept_client, args=(server,), daemon=True).start()
 
-    helper_msg = f'\nAvailiable commands: \n{HELP}\n{SHUTDOWN}\n{LIST}'
+    helper_msg = f'\nAvailiable commands: \n{consts.HELP}\n{consts.SHUTDOWN}\n{consts.LIST}'
     while True:
         match input():
-            case str(SHUTDOWN):
+            case str(consts.SHUTDOWN):
                 print('Shutdown!')
                 with users_lock:
                     for user in users.values():
-                        user.send(encode(DISCONNECT))
+                        user.send(encode(consts.DISCONNECT))
                         user.close()
                     break
-            case str(LIST):
-                print('LIST')
-            case str(HELP):
+            case str(consts.LIST):
+                print(users_enum())
+            case str(consts.HELP):
                 print(helper_msg)
             case _:
                 print(f'Unknown command!\n{helper_msg}')
